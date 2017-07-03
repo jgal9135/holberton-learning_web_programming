e26dad2d1e5a83126fe4d9c9e73c1052a58da73c
alexa <alexasoucy123@gmail.com>
2017-07-03 15:38:46 -0700

    changed position of brackets

--- 6-javascript_continued/3-grading_system.js
+++ 6-javascript_continued/3-grading_system.js
@@ -3,23 +3,18 @@ let parameter=process.argv[2];
 if (parameter >= 95) {
     console.log("You received an A+");
 }
-else {
-  (parameter >= 90)
+else if(parameter >= 90){
     console.log("You received an A");
 }
-else {
-  (parameter >= 85)
+else if(parameter >= 85){
     console.log("You received an B+");
 }
-else {
-  (parameter >= 80)
+else if(parameter >= 80){
     console.log("You received an B");
 }
-else {
-  (parameter >= 75)
+else if(parameter >= 75){
     console.log("You received an C+");
 }
-else {
-  (parameter >= 70)
+else if(parameter >= 70){
     console.log("You received an C");
 }
