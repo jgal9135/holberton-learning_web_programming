3d38e0f5bb72e59f1c3ad1b4696efd9b9b3ab0b5
alexa <alexasoucy123@gmail.com>
2017-07-03 15:42:11 -0700

    let -> var

--- 6-javascript_continued/3-grading_system.js
+++ 6-javascript_continued/3-grading_system.js
@@ -1,4 +1,4 @@
-let parameter=process.argv[2];
+var parameter=process.argv[2];
 
 if (parameter >= 95) {
     console.log("You received an A+");
@@ -7,14 +7,14 @@ else if(parameter >= 90){
     console.log("You received an A");
 }
 else if(parameter >= 85){
-    console.log("You received an B+");
+    console.log("You received a B+");
 }
 else if(parameter >= 80){
-    console.log("You received an B");
+    console.log("You received a B");
 }
 else if(parameter >= 75){
-    console.log("You received an C+");
+    console.log("You received a C+");
 }
 else if(parameter >= 70){
-    console.log("You received an C");
+    console.log("You received a C");
 }
