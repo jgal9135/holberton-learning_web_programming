b502a4c46c2904c082b96b6bbfc8444deaed5d2a
alexa <alexasoucy123@gmail.com>
2017-07-03 15:34:21 -0700

    grading system

---
+++ 6-javascript_continued/3-grading_system.js
@@ -0,0 +1,25 @@
+let parameter=process.argv[2];
+
+if (parameter >= 95) {
+    console.log("You received an A+");
+}
+else {
+  (parameter >= 90)
+    console.log("You received an A");
+}
+else {
+  (parameter >= 85)
+    console.log("You received an B+");
+}
+else {
+  (parameter >= 80)
+    console.log("You received an B");
+}
+else {
+  (parameter >= 75)
+    console.log("You received an C+");
+}
+else {
+  (parameter >= 70)
+    console.log("You received an C");
+}
