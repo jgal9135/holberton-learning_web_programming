8aedde87cd77e15ef6e403f7b1b05f0359468ed1
alexa <alexasoucy123@gmail.com>
2017-07-03 15:10:19 -0700

    else

--- 6-javascript_continued/2-what_else.js
+++ 6-javascript_continued/2-what_else.js
@@ -1,6 +1,9 @@
 let parameter=process.argv[2];
 
-if (parameter > 5)
-  console.log("The value is greater than 5");
-else if (parameter <= 5)
-  console.log("The value is equal to 5 or lower");
+if (parameter > 5) {
+    console.log("The value is greater than 5");
+}
+else {
+  (parameter <= 5)
+    console.log("The value is equal to 5 or lower");
+}
